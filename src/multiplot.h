64d98cb7f641102b84c8a2e6343af0bbb84a3096
Ethan A Merritt <merritt@u.washington.edu>
2023-11-15 15:12:19 -0800

    remultiplot - ignore problematic commands during multiplot record/playback
    Some commands (pause, reset, here-documents) would be problematic if
    executed during multiplot playback.  Filter these out in playback mode.
    
    load/call commands must not be stored into $GPVAL_LAST_MULTIPLOT as
    separate commans, since their contents were already stored line-by-line
    when first executed.
    
    reset commands, datablocks and function block definitions
    are ignored during playback.

--- src/multiplot.h
+++ src/multiplot.h
@@ -9,3 +9,12 @@ int multiplot_current_panel(void);
 
 extern TBOOLEAN multiplot_playback;	/* TRUE while inside "remultiplot" playback */
 extern TBOOLEAN multiplot_mouse;	/* use "remultiplot" for "replot" while mousing */
+
+/* Some commands (pause, reset, ...) would be problematic if executed during
+ * multiplot playback.  Invoke this from the command that should be filtered.
+ */
+#define filter_multiplot_playback() \
+	if (multiplot_playback) { \
+	    while (!END_OF_COMMAND) c_token++; \
+	    return; \
+	}
